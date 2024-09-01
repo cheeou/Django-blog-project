5b2b66d2d663a5d2096c2d70daa4f98a2f691542
cheeou <>
2024-09-01 17:49:11 +0900

    ✨ paginator for post list

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     ),
     path("logout/", views.logout_view, name="logout"),
     path("signup/", views.signup, name="signup"),
+    # path("profile/", views.profile, name="profile"),
 ]
--- main/views.py
+++ main/views.py
@@ -7,21 +7,20 @@ def index(request):
     return render(request, "main/index.html")
 
 
-# def main(request):  # 메인 화면
-#     posts = Post.objects.all()
-
-
-#     context = {
-#         "posts": posts,
-#     }
-#     return render(request, "main/main.html", context)
 def main(request):
+    # 내가 쓴 글 보기
     show_my_posts = request.GET.get("show_my_posts") == "true"
 
     if show_my_posts and request.user.is_authenticated:
-        posts = Post.objects.filter(author=request.user).order_by("-pub_date")
+        post_list = Post.objects.filter(author=request.user).order_by("-pub_date")
     else:
-        posts = Post.objects.all().order_by("-pub_date")
+        post_list = Post.objects.all().order_by("-pub_date")
+
+    # pagination
+    paginator = Paginator(post_list, 6)  # 한 페이지에 6개 출력
+    page_number = request.GET.get("page")
+    posts = paginator.get_page(page_number)
 
     context = {"posts": posts, "show_my_posts": show_my_posts}
+
     return render(request, "main/main.html", context)
